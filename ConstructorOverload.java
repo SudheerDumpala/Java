d0ece38ac9ec7e9010c5abf6e09d236fe452d03a
SudheerDumpala <sudheerdumpala123@gmail.com>
2024-07-27 16:13:26 +0530

    ConstructorOverload.java

--- ConstructorOverload.java
+++ ConstructorOverload.java
@@ -1,3 +1,7 @@
+//note: A constructor called for every time a new object is created for the class
+// A constructor look as same as method but the only difference is it doesn't have any return type
+// The name of the constructor is must be same as the name of the class
+
 public class ConstructorOverload {
     int id;
     String name;
