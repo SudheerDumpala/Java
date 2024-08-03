a3f9ed28e91fc21f65932c37eaa7152fe6e7db02
Sudheer Dumpala <sudheerdumpala123@gmail.com>
2024-08-03 23:09:30 +0530

    Add files via upload

---
+++ Abstraction.java
@@ -0,0 +1,31 @@
+/* Points to remember.
+ * Every abstract method which is declared in the abstraction class must be implemented
+ * in the same class or other class.
+ * an abstract class may or may not contain abstract methods, but an abstract method must be placed
+ * inside the abstract class
+ * It is necessary to define a class as abstract if we are declaring an abstract method.
+ * We can't able to create the class which is declared as abstracc. 
+ */
+abstract class Hide{
+    abstract public void hidden();
+    public void shown(){
+        System.out.println("This is a non-abstract shown() method");
+    }
+}
+abstract class Hide1 extends Hide{
+    @Override
+    public void hidden(){
+        System.out.println("This is an abstarct hidden() method");
+    }
+}
+class Hide2 extends Hide1{
+    public void hidden1(){
+        System.out.println("This is the non-abstract hidden1() method");
+    }
+}
+public class Abstraction{
+    public static void main(String[] args) {
+        Hide2 obj = new Hide2();
+        obj.hidden();
+    }
+}
