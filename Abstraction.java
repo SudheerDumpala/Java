9081ca6416c35f5425830261729bf195a1bd09b0
Sudheer Dumpala <sudheerdumpala123@gmail.com>
2024-08-06 16:34:20 +0530

    Abstraction.java

--- Abstraction.java
+++ Abstraction.java
@@ -1,10 +1,11 @@
 /* Points to remember.
- * Every abstract method which is declared in the abstraction class must be implemented
+ * Every abstract method which is declared in the abstract class must be implemented
  * in the same class or other class.
  * an abstract class may or may not contain abstract methods, but an abstract method must be placed
  * inside the abstract class
+ * The methods which have its implementation where it is defined (unlike abstract) are called "concreate methods"
  * It is necessary to define a class as abstract if we are declaring an abstract method.
- * We can't able to create the class which is declared as abstracc. 
+ * We can't able to create the object of the class which is declared as abstract. 
  */
 abstract class Hide{
     abstract public void hidden();
